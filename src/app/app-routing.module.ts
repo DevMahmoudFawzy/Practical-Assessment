9576c7985d7e169d4c045f19675483648508e493
Mahmoud Fawzy <dev.mahmoudfawzy@gmail.com>
2023-06-26 02:20:46 +0200

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,38 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { MainLayoutComponent } from './shared/layouts/main-layout/main-layout.component';
+import { HomeComponent } from './home/components/home/home.component';
+import { PageNotFoundComponent } from './shared/components/page-not-found/page-not-found.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: MainLayoutComponent,
+    children: [
+      {
+        path: '',
+        component: HomeComponent,
+        data: { breadCrumb: 'Home' }
+      },
+      {
+        path: 'products',
+        loadChildren: () => import('./product/product.module').then(m => m.ProductModule),
+        // canActivate: [AuthGuard],
+      },
+      {
+        path: 'orders',
+        loadChildren: () => import('./order/order.module').then(m => m.OrderModule),
+        // canActivate: [AuthGuard],
+      },
+      {
+        path: 'customers',
+        loadChildren: () => import('./customer/customer.module').then(m => m.CustomerModule)
+      }
+    ]
+  },
+  { path: '**', component: PageNotFoundComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,32 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainLayoutComponent } from './shared/layouts/main-layout/main-layout.component';
+import { MainSideNavComponent } from './shared/layouts/main-layout/main-side-nav/main-side-nav.component';
+import { MainHeaderComponent } from './shared/layouts/main-layout/main-header/main-header.component';
+import { MainFooterComponent } from './shared/layouts/main-layout/main-footer/main-footer.component';
+import { PageNotFoundComponent } from './shared/components/page-not-found/page-not-found.component';
+import { GenericSideNavComponent } from './shared/components/generic-side-nav/generic-side-nav.component';
+import { BaseComponent } from './shared/components/base/base.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainLayoutComponent,
+    MainSideNavComponent,
+    MainHeaderComponent,
+    MainFooterComponent,
+    PageNotFoundComponent,
+    GenericSideNavComponent,
+    BaseComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
